2f635f7cccd87640c148ea8f1ff48b83d42f8470
Kirill Rogovoy <kirill@rogovoy.me>
2016-11-20 21:58:27 +0200

    Fix ln usage in apply_konfig.sh

--- apply_konfig.sh
+++ apply_konfig.sh
@@ -3,10 +3,14 @@ set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 for dir in $(ls -A "$DIR/home"); do
-    if [ -d "$HOME/$dir" ]; then
-        ls -A "$DIR/home/$dir" | xargs -I "%" ln -sfFh "$DIR/home/$dir/%" "$HOME/$dir/%"
+    if [ -d "$HOME/$dir" ] && ! [ -L "$HOME/$dir" ]; then
+        for subdir in $(ls -A "$DIR/home/$dir"); do
+            rm -f "$HOME/$dir/$subdir"
+            ln -s "$DIR/home/$dir/$subdir" "$HOME/$dir/$subdir"
+        done
     else
-        ln -sfFh "$DIR/home/$dir" "$HOME/$dir"
+        rm -f "$HOME/$dir"
+        ln -s "$DIR/home/$dir" "$HOME/$dir"
     fi
 done
 
