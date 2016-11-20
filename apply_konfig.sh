ecd9a7bf8f6688753573df1e605155d5224dbc2c
Kirill Rogovoy <kirill@rogovoy.me>
2016-11-20 20:46:51 +0200

    Fix apply_konfig symbolic links recreation

--- apply_konfig.sh
+++ apply_konfig.sh
@@ -3,10 +3,11 @@ set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 for dir in $(ls -A "$DIR/home"); do
-    echo $dir
     if [ -d "$HOME/$dir" ]; then
-        ls -A "$DIR/home/$dir" | xargs -I "%" ln -sfF "$DIR/home/$dir/%" "$HOME/$dir/%"
+        ls -A "$DIR/home/$dir" | xargs -I "%" ln -sfFh "$DIR/home/$dir/%" "$HOME/$dir/%"
     else
-        ln -sfF "$DIR/home/$dir" "$HOME/$dir"
+        ln -sfFh "$DIR/home/$dir" "$HOME/$dir"
     fi
 done
+
+echo OK
