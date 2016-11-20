75d845f7b40bb8db74a4a3c6ef54dfa5561355a7
Kirill Rogovoy <kirill@rogovoy.me>
2016-11-20 22:00:40 +0200

    Fix rm usage in apply_konfig.sh

--- apply_konfig.sh
+++ apply_konfig.sh
@@ -5,11 +5,11 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 for dir in $(ls -A "$DIR/home"); do
     if [ -d "$HOME/$dir" ] && ! [ -L "$HOME/$dir" ]; then
         for subdir in $(ls -A "$DIR/home/$dir"); do
-            rm -f "$HOME/$dir/$subdir"
+            rm -rf "$HOME/$dir/$subdir"
             ln -s "$DIR/home/$dir/$subdir" "$HOME/$dir/$subdir"
         done
     else
-        rm -f "$HOME/$dir"
+        rm -rf "$HOME/$dir"
         ln -s "$DIR/home/$dir" "$HOME/$dir"
     fi
 done
