8c2e98efc31559beec684ad3be2bc42e31e6b505
Kirill Rogovoy <kirill@rogovoy.me>
2017-07-14 12:52:55 +0300

    Change prompt to a random emoji

--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -1,4 +1,6 @@
 function fish_prompt
   set_color -o red
-  echo -n -s "> "
+  set -l filepath ~/.config/fish/functions/emoji.txt
+  set -l random_emoji (bash -c "head -\$((\${RANDOM} % `wc -l < $filepath` + 1)) $filepath | tail -1")
+  echo -n -s -e "\n$random_emoji  "
 end
